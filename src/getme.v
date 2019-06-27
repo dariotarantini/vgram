079ade7f063f8390f53af1fb987d88fdec283c14
Dario Tarantini <dariotarantinimail@gmail.com>
2019-06-27 18:43:04 +0200

    4red

---
+++ src/getme.v
@@ -0,0 +1,24 @@
+module hygram
+
+import http
+import json
+
+struct getMeResp {
+mut:
+    id int
+    is_bot bool
+    first_name string
+    username string
+}
+struct resultGetMe {
+mut:
+    ok bool
+    result getMeResp
+}
+pub fn (u Telegram) get_me(offset int) resultGetUpdates {
+    resp := json.decode(resultGetMe, u.raw_request('getMe', {})) or {
+	    eprintln('Failed to decode json')
+	    return resultGetUpdates{}
+    }
+    return resp
+}
