b34d9f74996a414266d0f12ef5a505d662fadd36
Dario Tarantini <dariotarantinimail@gmail.com>
2019-07-24 14:27:32 +0200

    Update vgram.v

--- src/vgram.v
+++ src/vgram.v
@@ -3,38 +3,27 @@ module vgram
 import http
 import json
 
-
-// main struct
-struct Telegram {
-pub:
+struct Bot {
     Token string
     Debug bool
 }
-
-pub fn new_bot(utoken string, udebug bool) Telegram {
-    return Telegram{
-        Token: utoken
-        Debug: udebug
+pub fn new_bot(token string, debug bool) Bot {
+    return Bot{
+        Token: token
+        Debug: debug
     }
 }
-pub fn (u Telegram) send(method string, data map[string]string) string {
-    mut post_data := ''
 
-    //for e in data.entries {
-    //    k := e.key
-    //    v := data[k]
-    //    post_data += '$e.key=$v&'
-    //}
-
-    for key, value in data {
-        post_data += '$key=$value+'
-    }
-    url := 'https://api.telegram.org/bot'+u.Token+'/$method'
-    if u.Debug == true {
+fn (d Bot) http_request(data string) APIResponse {
+    url := 'https://api.telegram.org/bot'+d.Token+'/'
+    if d.Debug == true {
         println('--- DEBUG ---')
-        println('URL: $url')
-        println('POST: $post_data')
+        println('POST: $data')
         println('--- END ---')
     }
-    return http.post(url, post_data)
+    result := json.decode(APIResponse, http.post(url, data)) or { 
+        panic('failed to decode json')
+        return APIResponse{}
+    }
+    return result
 }
