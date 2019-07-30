b42b1f991041b70d7be3370ae390ba52ef5e19d6
Dario Tarantini <dariotarantinimail@gmail.com>
2019-07-30 13:24:25 +0200

    fix http

--- vgram.v
+++ vgram.v
@@ -21,11 +21,13 @@ fn (d Bot) http_request(data string) APIResponse {
         println('POST: $data')
         println('--- END ---')
     }
-    str_resp := http.post(url, data)
-    result := json.decode(APIResponse, str_resp)
-    // or { 
-    //    panic('failed to decode json')
-    //    return APIResponse{}
-    //}
+    str_resp := http.post(url, data) or { 
+        panic('failed to make http req')
+        return APIResponse{}
+    }
+    result := json.decode(APIResponse, str_resp) or { 
+        panic('failed to decode json')
+        return APIResponse{}
+    }
     return result
 }
