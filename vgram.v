82ca6285a35e348ec5c1fb55ce42e699c456cb63
Dario Tarantini <dariotarantinimail@gmail.com>
2019-08-04 12:38:55 +0200

    Update vgram.v

--- vgram.v
+++ vgram.v
@@ -4,26 +4,26 @@ import http
 import json
 
 struct Bot {
-    Token string
-    Debug bool
+    token string
+    debug bool
 }
-pub fn new_bot(token string, debug bool) Bot {
+    pub fn new_bot(utoken string, udebug bool) Bot {
     return Bot{
-        Token: token
-        Debug: debug
+        token: utoken
+        debug: udebug
     }
 }
 
 fn (d Bot) http_request(data string) ApiResponse {
-    url := 'https://api.telegram.org/bot'+d.Token+'/'
-    if d.Debug == true {
+    url := 'https://api.telegram.org/bot'+d.token+'/'
+    if d.debug == true {
         println('--- DEBUG ---')
         println('POST: $data')
         println('--- END ---')
     }
     str_resp := http.post(url, data) or { 
         panic('failed to make http req')
-        return APIResponse{}
+        return ApiResponse{}
     }
     result := json.decode(ApiResponse, str_resp.text) or { 
         panic('failed to decode json')
