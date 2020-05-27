44a3f1339275ff90ad485cac3d06220167bd7b46
Dario Tarantini <dario@debian>
2020-05-27 22:14:33 +0200

    fix

--- vgram.v
+++ vgram.v
@@ -15,36 +15,36 @@ pub fn new_bot(utoken string) Bot {
     }
 }
 
-fn (d Bot) http_request(method, _data string) string {
-    _req := http.Request{
+fn (d Bot) http_request(method, xdata string) string {
+    xreq := http.Request{
         method: "POST"
         headers: {
             'Content-Type': 'application/json'
         }
         cookies: map[string]string
-        data: _data
+        data: xdata
         url: "https://api.telegram.org/bot${d.token}/${method}"
         user_agent: ""
         verbose: false
         user_ptr: 0
         ws_func: 0
     }
-    result := _req.do() or {
+    result := xreq.do() or {
         println("Unable to do request")
         return ""
     }
     if result.status_code == 200 {
-        _tgresp := json.decode(ResponserOK, result.text) or { 
+        xtgresp := json.decode(ResponserOK, result.text) or { 
             println("Failed to decode json")
             return ""
         }
-    	return _tgresp.result
+    	return xtgresp.result
     } else {
-        _tgresp := json.decode(ResponserNotOK, result.text) or { 
+        xtgresp := json.decode(ResponserNotOK, result.text) or { 
             println("Failed to decode json")
             return ""
         }
-        println("\n\nError!\nMethod: ${method}\nTime: " + time.now().str() + "\nData: ${_data}\nError code: " + _tgresp.error_code.str() + "\nDescription: " + _tgresp.description)
+        println("\n\nError!\nMethod: ${method}\nTime: " + time.now().str() + "\nData: ${xdata}\nError code: " + xtgresp.error_code.str() + "\nDescription: " + xtgresp.description)
         return ""
     }
 }
